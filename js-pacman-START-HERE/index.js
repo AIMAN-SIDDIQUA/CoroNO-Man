b3f6b4afb700b89576dfe6458f7b383c1d6b85d7
AIMAN SIDDIQUA <>
2021-04-10 13:46:06 +0530

    first commit

---
+++ js-pacman-START-HERE/index.js
@@ -0,0 +1,65 @@
+import { LEVEL, OBJECT_TYPE } from './setup';
+
+//classes
+import GameBoard from './GameBoard';
+import Pacman from './Pacman';
+
+// Dom Elements
+const gameGrid = document.querySelector('#game');
+const scoreTable = document.querySelector('#score');
+const startButton = document.querySelector('#start-button');
+
+// Game constants
+const POWER_PILL_TIME = 10000; // ms
+const GLOBAL_SPEED = 80; // ms
+const gameBoard = GameBoard.createGameBoard(gameGrid, LEVEL);
+
+// Initial setup
+let score = 0;
+let timer = null;
+let gameWin = false;
+let powerPillActive = false;
+let powerPillTimer = null;
+
+function gameOver(pacman, grid){
+
+}
+
+function checkCollision(pacman, ghosts){
+
+}
+
+function gameLoop(pacman, ghosts){
+	console.log("works!");
+	gameBoard.moveCharacter(pacman);
+}
+
+function startGame(){
+	gameWin = false;
+	powerPillActive = false;
+	  score = 0;
+
+	  startButton.classList.add('hide');
+
+	  gameBoard.createGrid(LEVEL);
+
+	  const pacman = new Pacman(2, 209);
+	  gameBoard.addObject(209, [OBJECT_TYPE.PACMAN]);
+
+	  document.addEventListener('keydown', (e) =>
+	    pacman.handleKeyInput(e, gameBoard.objectExist.bind(gameBoard))
+	  );
+
+	  /*const ghosts = [
+	    new Ghost(5, 188, randomMovement, OBJECT_TYPE.BLINKY),
+	    new Ghost(4, 209, randomMovement, OBJECT_TYPE.PINKY),
+	    new Ghost(3, 230, randomMovement, OBJECT_TYPE.INKY),
+	    new Ghost(2, 251, randomMovement, OBJECT_TYPE.CLYDE)
+	  ];*/
+
+	  // Gameloop
+	  timer = setInterval(() => gameLoop(pacman), GLOBAL_SPEED);
+}
+
+//Initialize game 
+startButton.addEventListener('click', startGame);
